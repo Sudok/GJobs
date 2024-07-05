c09a61aa9d7e4c1d0c35a03d387a6d769a438a3b
Glauber Reis <glauber.gw@gmail.com>
2024-07-05 10:56:06 -0300

    feat: adjusting rules for applications in submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resources :jobs
 
+  resources :submissions
+
   namespace :recruiter do
     mount_devise_token_auth_for 'Recruiter', at: 'auth', as: 'login'
-  end
-
-  namespace :publica do
-    resources :submissions
   end
 end
